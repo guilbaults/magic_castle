7ae2ac386289724d3ae3b0b3a8a9c8f6a36ecebb
Simon Guilbault <simon.guilbault@calculquebec.ca>
2020-02-13 10:04:38 -0500

    Inserting terraform variables in each functions

--- tests/devstack/common.sh
+++ tests/devstack/common.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-export LOGIN_IPADDR=$(jq -j '.outputs.public_ip.value[0]' terraform.tfstate)
-export GUEST_PASSWD=$(jq -j '.outputs.guest_passwd.value' terraform.tfstate)
-
 function ssh_user {
+  LOGIN_IPADDR=$(jq -j '.outputs.public_ip.value[0]' terraform.tfstate)
+  GUEST_PASSWD=$(jq -j '.outputs.guest_passwd.value' terraform.tfstate)
   echo -n "$(sshpass -p "${GUEST_PASSWD}" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null user01@${LOGIN_IPADDR} "$1" )"
 }
 
 function ssh_centos_login1 {
-  echo -n "LOGIN_IPADDR=${LOGIN_IPADDR}"
+  LOGIN_IPADDR=$(jq -j '.outputs.public_ip.value[0]' terraform.tfstate)
   echo -n "$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null centos@${LOGIN_IPADDR} "$1" )"
 }
 
 function ssh_centos_mgmt1 {
+  LOGIN_IPADDR=$(jq -j '.outputs.public_ip.value[0]' terraform.tfstate)
   echo -n "$(ssh -A -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null centos@${LOGIN_IPADDR} "ssh centos@mgmt1 $1" )"
 }
 
 function ssh_centos_node1 {
+  LOGIN_IPADDR=$(jq -j '.outputs.public_ip.value[0]' terraform.tfstate)
   echo -n "$(ssh -A -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null centos@${LOGIN_IPADDR} "ssh centos@node1 $1" )"
 }
 
